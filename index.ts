22767a6ccd6c262dab0d93f6ef7e36cae9fa0bca
Ania-beigi <anahita.einbeigi@gmail.com>
2025-09-04 12:21:27 +0200

    update environment

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,               // smtp.gmail.com
   port: Number(process.env.SMTP_PORT),       // 587
-  secure: true,                             // STARTTLS
+  secure: false,                             // STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
